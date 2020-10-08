38c085f1231690751ac4f2571341d512127eef52
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-10-08 15:24:50 -0400

    added folder and file architecture for the app.

--- server.js
+++ server.js
@@ -6,12 +6,34 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//VIEWS ROUTES
+
+app.get("/", (req, res) => {
+    connection.query("SELECT * FROM ...", (err,data) => {
+        console.table(data);
+    })
+    res.render("index", {name: "Ryan"});
+});
+
+//API ROUTEs
+
 app.get("/api/config", (req,res) => {
     res.json ({
         success: true,
     });
 });
 
+
+
+
+
+//APP LISTENER
 app.listen(PORT,  () => {
     console.log(`server is running on http://localhost:${PORT}`);
 });
