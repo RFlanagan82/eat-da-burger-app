24f6a1e64d128ae26707cce2524050ecc3190f90
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-10-08 15:38:06 -0400

    added JAWSDB_URL code to connection.js

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+  
+const mysql = require("mysql");
+
+let connection;
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else{
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        port: "3306",
+        password: "Alucard1",
+        database: "burger_db"
+    })
+}
+
+connection.connect();
+
+module.exports = connection;
