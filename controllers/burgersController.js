3d12c386ee7b9d26e7d616f0246fb18a0bcedaad
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-10-08 22:00:58 -0400

    modified server.js and did mysql integration

---
+++ controllers/burgersController.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+const burger = require("../models/burger.js");
+
+
+
+
+//Export routes for server.js to use
+module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const mysql = require("mysql");
 const connection = require("./config/connection")
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+//Serve static content for the app from the 'public' directory in the application directory
+app.use(express.static("public"));
+
+//Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Set Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 //VIEWS ROUTES
 
 app.get("/", (req, res) => {
-    connection.query("SELECT * FROM burger_db", (err,data) => {
+    connection.query("SELECT * FROM burger", (err,data) => {
         console.table(data);
     })
     res.render("index");
